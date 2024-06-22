b3063d6029b1ad49013c69fa38936d3876735858
paulfersi <paolfersi@gmail.com>
2024-06-22 19:00:38 +0200

    implemented functionnality to add users via admin page

--- core/admin.py
+++ core/admin.py
@@ -2,11 +2,16 @@
 
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from .models import Profile
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     # only username
     fields = ["username"]
+    inlines = [ProfileInline]   #modify them in the same place
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
