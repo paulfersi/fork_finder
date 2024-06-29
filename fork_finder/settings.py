a4e76a9eaae988bf0d8bc6d3251201a5df98e008
paulfersi <paolfersi@gmail.com>
2024-06-29 17:07:33 +0200

    typos

--- fork_finder/settings.py
+++ fork_finder/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/js/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/js")]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 # Default primary key field type
