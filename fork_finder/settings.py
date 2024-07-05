105e01cf8e087b74c7b3368712a978e5e0a0d824
paulfersi <paolfersi@gmail.com>
2024-07-06 01:16:55 +0200

    fixed login

--- fork_finder/settings.py
+++ fork_finder/settings.py
@@ -135,4 +135,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/feed/"
+LOGOUT_REDIRECT_URL = "/"
