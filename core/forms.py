5c9d3767d6a19d9b28ca7dc9f396a5821fa3fed4
paulfersi <paolfersi@gmail.com>
2024-07-05 09:15:56 +0200

    search box now works

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,5 @@
 from django import forms
 from .models import Review
 
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        fields = ['restaurant', 'body', 'rating', 'photo']
-        widgets = {
-            'restaurant': forms.HiddenInput()
-        }
+class ReviewForm(forms.Form):
+    review = forms.CharField(widget=forms.Textarea, label='Your Review')
--- core/views.py
+++ core/views.py
@@ -6,8 +6,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.views import View
-from django.conf import settings
 from django.urls import reverse_lazy
+import json
+from django.conf import settings
+
+MAPBOX_TOKEN = settings.MAPBOX_ACCESS_TOKEN
 
 @login_required
 def feed_view(request):
@@ -46,12 +49,30 @@ def search_user_view(request):
             return render(request, 'user_not_found.html', {'query': query})
     return redirect('feed')
 
-
 class AddReviewView(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         form = ReviewForm()
-        return render(request, 'add_review.html', {
-            'form': form,
-            'mapbox_access_token': settings.MAPBOX_ACCESS_TOKEN
-        })
-    
+        return render(request, 'add_review.html', {'form': form, 'mapbox_access_token': MAPBOX_TOKEN})
+
+    def post(self, request, *args, **kwargs):
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            selected_place = json.loads(request.POST.get('selected_place', '{}'))
+            place_id = selected_place.get('place_id')
+            name = selected_place.get('name')
+            location = selected_place.get('location')
+
+            restaurant, created = Restaurant.objects.get_or_create(
+                place_id=place_id,
+                defaults={'name': name, 'location': location}
+            )
+
+            review = form.save(commit=False)
+            # Assume a user is assigned here, for example:
+            review.user = request.user  # Modify this line as needed based on your application logic
+            review.restaurant = restaurant
+            review.save()
+
+            return redirect('feed')  # Redirect to a success page or another appropriate page
+
+        return render(request, 'add_review.html', {'form': form, 'mapbox_access_token': MAPBOX_TOKEN})
