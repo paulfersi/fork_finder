8b1808dc190c6c6c6748a7f73ac64eacd78f121b
paulfersi <paolfersi@gmail.com>
2024-07-06 23:54:47 +0200

    typos

--- fork_finder/settings.py
+++ fork_finder/settings.py
@@ -136,4 +136,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/feed/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/feed/"
