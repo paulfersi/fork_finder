79cc27d057f037ed2221a8c2058fcc16a321a980
paulfersi <paolfersi@gmail.com>
2024-07-07 08:35:45 +0200

    improved get_location logic

--- fork_finder/settings.py
+++ fork_finder/settings.py
@@ -135,5 +135,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/feed/"
+LOGIN_REDIRECT_URL = "/get_location/"
 LOGOUT_REDIRECT_URL = "/feed/"
